cacbfcc5850c656acc649357f9e787791609736b
D·Teach <1114626450@qq.com>
2020-10-08 22:49:59 +0800

    API网关

---
+++ mi-gateway/src/test/java/com/dlq/mall/gateway/MiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlq.mall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
