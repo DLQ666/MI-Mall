abc80321a45def6fb1ba02b13eac582c37fbe560
D·Teach <1114626450@qq.com>
2020-11-12 22:41:00 +0800

    ES检索微服务

--- mi-search/src/test/java/com/dlq/mall/search/MiSearchApplicationTests.java
+++ mi-search/src/test/java/com/dlq/mall/search/MiSearchApplicationTests.java
@@ -3,10 +3,24 @@ package com.dlq.mall.search;
 import com.alibaba.fastjson.JSON;
 import com.dlq.mall.search.confg.ESConfig;
 import lombok.Data;
+import lombok.ToString;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,17 +28,91 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.annotation.Async;
 
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest
 class MiSearchApplicationTests {
 
     @Autowired
     RestHighLevelClient restHighLevelClient;
+    
+    //复杂检索
+    @Test
+    public void searchData() throws IOException {
+        //1、创建检索请求
+        SearchRequest searchRequest = new SearchRequest();
+        //指定索引
+        searchRequest.indices("bank");
+        //指定DSL,检索条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        //1.1、构造检索条件
+//        sourceBuilder.from();
+//        sourceBuilder.size();
+//        sourceBuilder.aggregation();
+        sourceBuilder.query(QueryBuilders.matchQuery("address","mill"));
+
+        //1.2、按照年龄的值分布进行聚合
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        sourceBuilder.aggregation(ageAgg);
+        //1.3、计算平均薪资
+        AvgAggregationBuilder balanceAvg = AggregationBuilders.avg("balanceAvg").field("balance");
+        sourceBuilder.aggregation(balanceAvg);
+        System.out.println("封装的检索条件"+sourceBuilder.toString());
+        searchRequest.source(sourceBuilder);
+
+        //2、执行检索
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, ESConfig.COMMON_OPTIONS);
+
+        //3、分析结果 searchResponse
+        System.out.println(searchResponse.toString());
+        //3.1、获取所有查到的记录
+        SearchHits hits = searchResponse.getHits();
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit : searchHits) {
+            /**
+             * "_index" : "bank",
+             *         "_type" : "account",
+             *         "_id" : "970",
+             *         "_score" : 5.5761433,
+             *         "_source"
+             */
+            String sourceAsString = hit.getSourceAsString();
+            Account account = JSON.parseObject(sourceAsString, Account.class);
+            System.out.println("account"+account);
+        }
+        //3.2、获取聚合的分析信息
+        Aggregations aggs = searchResponse.getAggregations();
+        Terms agg = aggs.get("ageAgg");
+        List<? extends Terms.Bucket> buckets = agg.getBuckets();
+        for (Terms.Bucket bucket : buckets) {
+            String keyAsString = bucket.getKeyAsString();
+            System.out.println("分析的年龄信息："+keyAsString+"==>"+bucket.getDocCount());
+        }
+
+        Avg balance = aggs.get("balanceAvg");
+        System.out.println("平均薪资："+balance.getValue());
+    }
+
+    @ToString
+    @Data
+    static class Account{
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private int age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+    }
 
     //测试存储数据到es
     //更新也可以
     @Test
-    public void testIndexData() throws IOException {
+    public void indexData() throws IOException {
         IndexRequest indexRequest = new IndexRequest("users");
         indexRequest.id("1");//数据的id
 //        indexRequest.source("userName","zhangsan","age",18,"gender","男");
