69dd6b8940878fbfe2baa4ea5a7c6c6d41b7fbce
D·Teach <1114626450@qq.com>
2020-11-20 23:32:31 +0800

    商品服务

---
+++ mi-product/src/main/java/com/dlq/mall/product/feign/WareFeignService.java
@@ -0,0 +1,28 @@
+package com.dlq.mall.product.feign;
+
+import com.dlq.common.to.SkuHasStockVo;
+import com.dlq.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ *@description:
+ *@author: Hasee
+ *@create: 2020-11-19 20:30
+ */
+@FeignClient("mi-ware")
+public interface WareFeignService {
+
+    /**
+     * 1、R设计的时候可以加上泛型
+     * 2、直接返回我们想要的结果
+     * 3、自己封装解析结果
+     * @param skuIds
+     * @return
+     */
+    @PostMapping("/ware/waresku/hasstock")
+    R getSkuHasStock(@RequestBody List<Long> skuIds);
+}
