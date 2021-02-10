864cfd5d5e0b54c5bfe9e58eae8cefd4d9fb541f
D·Teach <1114626450@qq.com>
2021-02-10 13:29:16 +0800

    初始化购物车服务

---
+++ mi-cart/src/test/java/com/dlq/cart/MiCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlq.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
