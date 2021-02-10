89c39c18fa25d77bf8ae3fb8f21d2c128b88c704
D·Teach <1114626450@qq.com>
2021-02-10 13:40:21 +0800

    添加公共依赖工具

---
+++ mi-common/src/main/java/com/dlq/common/constant/AuthServerConstant.java
@@ -0,0 +1,12 @@
+package com.dlq.common.constant;
+
+/**
+ *@program: MI-Mall
+ *@description:
+ *@author: Hasee
+ *@create: 2021-02-07 15:31
+ */
+public class AuthServerConstant {
+
+    public static final String SMS_CODE_CACHE_PREFIX = "sms:code:";
+}
