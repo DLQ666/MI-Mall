1ec258bca6368532016f3a0c0d44a3ddaf579a3d
D·Teach <1114626450@qq.com>
2021-02-12 23:37:22 +0800

    添加通用常量参数

--- mi-common/src/main/java/com/dlq/common/constant/AuthServerConstant.java
+++ mi-common/src/main/java/com/dlq/common/constant/AuthServerConstant.java
@@ -9,4 +9,6 @@ package com.dlq.common.constant;
 public class AuthServerConstant {
 
     public static final String SMS_CODE_CACHE_PREFIX = "sms:code:";
+
+    public static final String LOGIN_USER = "loginUser";
 }
