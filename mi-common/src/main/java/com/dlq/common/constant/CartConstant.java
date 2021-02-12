b0dcc2532526519ac39264dae8416942a9f02516
D·Teach <1114626450@qq.com>
2021-02-12 23:31:11 +0800

    添加购物车常量参数

---
+++ mi-common/src/main/java/com/dlq/common/constant/CartConstant.java
@@ -0,0 +1,12 @@
+package com.dlq.common.constant;
+
+/**
+ *@program: MI-Mall
+ *@description:
+ *@author: Hasee
+ *@create: 2021-02-11 21:01
+ */
+public class CartConstant {
+
+    public static final String TEMP_USER_COOKIE_NAME = "user-key";
+}
