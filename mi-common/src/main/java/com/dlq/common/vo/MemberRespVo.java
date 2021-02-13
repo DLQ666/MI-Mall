1437e31f6493eb7ee909755af9f23e3723890d78
D·Teach <1114626450@qq.com>
2021-02-13 21:06:30 +0800

    通用工具类

---
+++ mi-common/src/main/java/com/dlq/common/vo/MemberRespVo.java
@@ -0,0 +1,92 @@
+package com.dlq.common.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ *@program: MI-Mall
+ *@description:
+ *@author: Hasee
+ *@create: 2021-02-13 15:27
+ */
+@Data
+public class MemberRespVo {
+
+    private Long id;
+    /**
+     * 会员等级id
+     */
+    private Long levelId;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 昵称
+     */
+    private String nickname;
+    /**
+     * 手机号码
+     */
+    private String mobile;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 头像
+     */
+    private String header;
+    /**
+     * 性别
+     */
+    private Integer gender;
+    /**
+     * 生日
+     */
+    private Date birth;
+    /**
+     * 所在城市
+     */
+    private String city;
+    /**
+     * 职业
+     */
+    private String job;
+    /**
+     * 个性签名
+     */
+    private String sign;
+    /**
+     * 用户来源
+     */
+    private Integer sourceType;
+    /**
+     * 积分
+     */
+    private Integer integration;
+    /**
+     * 成长值
+     */
+    private Integer growth;
+    /**
+     * 启用状态
+     */
+    private Integer status;
+    /**
+     * 注册时间
+     */
+    private Date createTime;
+
+    /**
+     * 社交用户唯一id
+     */
+    private String socialUid;
+    private String accessToken;
+    private Long expiresIn;
+}
