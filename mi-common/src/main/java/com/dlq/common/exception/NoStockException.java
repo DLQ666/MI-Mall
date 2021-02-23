65e504e29ddfada90a62764c657706321336daf5
D·Teach <1114626450@qq.com>
2021-02-24 00:02:48 +0800

    添加商品库存异常

--- mi-common/src/main/java/com/dlq/common/exception/NoStockException.java
+++ mi-common/src/main/java/com/dlq/common/exception/NoStockException.java
@@ -9,6 +9,9 @@ package com.dlq.common.exception;
 public class NoStockException extends RuntimeException{
 
     private Long skuId;
+    public NoStockException(String msg){
+        super(msg);
+    }
     public NoStockException(Long skuId){
         super("商品id："+skuId+"；没有足够的库存了");
     }
