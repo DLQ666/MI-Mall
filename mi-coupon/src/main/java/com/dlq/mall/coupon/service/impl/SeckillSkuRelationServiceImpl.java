3baaa4218ac5e6e7019a5629cf850f3764bb1c21
D·Teach <1114626450@qq.com>
2021-03-01 12:28:14 +0800

    初始化秒杀微服务

--- mi-coupon/src/main/java/com/dlq/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ mi-coupon/src/main/java/com/dlq/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -11,6 +11,7 @@ import com.dlq.common.utils.Query;
 import com.dlq.mall.coupon.dao.SeckillSkuRelationDao;
 import com.dlq.mall.coupon.entity.SeckillSkuRelationEntity;
 import com.dlq.mall.coupon.service.SeckillSkuRelationService;
+import org.springframework.util.StringUtils;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +19,15 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<SeckillSkuRelationEntity> queryWrapper = new QueryWrapper<>();
+        String promotionSessionId = (String) params.get("promotionSessionId");
+        if (!StringUtils.isEmpty(promotionSessionId)){
+            queryWrapper.eq("promotion_session_id", promotionSessionId);
+        }
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
---
+++ mi-seckill/src/test/java/com/dlq/seckill/MiSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlq.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
